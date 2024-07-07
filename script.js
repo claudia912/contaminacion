36fad3c64ad1f9b8feb2ce60661678b0eaa7feb2
claudia912 <211023645@prepaenlinea-sep.edu.mx>
2024-07-07 09:51:09 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Script para desplazamiento suave al hacer clic en los enlaces de navegación
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
